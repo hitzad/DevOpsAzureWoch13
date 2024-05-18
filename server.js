508110148dd4590c85d54dea4c29124beddaa66a
hitzad <hitzadr1@students.zhaw.ch>
2024-05-18 11:15:18 +0200

    neuer versuch

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Dieser Weg, wird kein leichter sein, dieser Weg ist steinig und schwer, jedoch nicht unmöglich');
+  res.send('Dieser Weg, wird kein leichter sein, dieser Weg ist steinig und schwer, jedoch nicht unmöglich, neuer versuch');
 });
 
 app.listen(PORT, HOST);
