2610fa1b2b530caa50e02435e852bb0af4311d58
hitzad <hitzadr1@students.zhaw.ch>
2024-05-18 10:44:54 +0200

    change fs24 week 13 hitz

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps FS2024 :-)))');
+  res.send('Dieser Weg, wird kein leichter sein, dieser Weg ist steinig und schwer');
 });
 
 app.listen(PORT, HOST);
